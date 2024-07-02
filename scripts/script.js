d0cbe54a86bf400ec60dce4f85818c84aceb5bf9
zoumushi <zoumushi@tutamail.com>
2024-07-02 23:59:31 +0200

    Forgot to commit often

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,37 @@
-console.log(`test`);
+const container = document.getElementById("container");
+const button = document.getElementById("grid-button");
+
+function changeGridColor(event) {
+	if (event.target.className === "column") {
+		event.target.style[`background-color`] = "black";
+	}
+}
+
+function createGrid(size) {
+	container.innerHTML = '';
+	for (i = 0; i < size; i++) {
+		const row = document.createElement("div");
+		row.setAttribute("class", "row");
+		for (j = 0; j < size; j++) {
+			const column = document.createElement("div");
+			column.setAttribute("class", "column");
+			row.appendChild(column);
+		}
+		container.appendChild(row);
+	}
+}
+
+function promptUser() {
+	let userNumber;
+	do {
+		userNumber = Number(prompt("enter number from 1 to 100"));
+	} while (userNumber > 100);
+	if (typeof userNumber === "number" && userNumber > 0) {
+		createGrid(userNumber)
+	}
+}
+
+createGrid(16);
+
+container.addEventListener("mouseover", changeGridColor);
+button.addEventListener("click", promptUser)
